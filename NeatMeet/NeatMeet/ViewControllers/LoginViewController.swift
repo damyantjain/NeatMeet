f027a5aa6d5cf1c713c01fe7ef3a5c810da2a58b
Esha <eshac7@gmail.com>
2024-11-19 22:04:21 -0500

    login register plus firebase

---
+++ NeatMeet/NeatMeet/ViewControllers/LoginViewController.swift
@@ -0,0 +1,121 @@
+//
+//  LoginViewController.swift
+//  NeatMeet
+//
+//  Created by Esha Chiplunkar on 10/31/24.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+
+    let loginView = LoginView()
+    let notificationCenter = NotificationCenter.default
+    var onLoginSuccess: (() -> Void)?
+    
+    override func loadView() {
+        view = loginView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "NeatMeet"
+        self.navigationItem.hidesBackButton = true
+        
+        self.view.backgroundColor = .white
+        
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboardOnTap))
+        tapRecognizer.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapRecognizer)
+        
+        loginView.loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        loginView.registerButton.addTarget(self, action: #selector(registerButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func hideKeyboardOnTap(){
+        view.endEditing(true)
+    }
+    
+    @objc func registerButtonTapped() {
+        let registerVC = RegisterViewController()
+        navigationController?.pushViewController(registerVC, animated: true)
+    }
+    
+    @objc func loginButtonTapped() {
+        if let emailText = loginView.emailText.text?.trimmingCharacters(
+                in: .whitespacesAndNewlines),
+                let passwordText = loginView.passwordText.text?.trimmingCharacters(
+                    in: .whitespacesAndNewlines)
+            {
+
+                if !isValidEmail(emailText) {
+                    showAlert(
+                        title: "Invalid Email!",
+                        message: "Please enter a valid email address.")
+                    return
+                }
+
+                if passwordText.isEmpty {
+                    showAlert(
+                        title: "Password cannot be empty!",
+                        message: "Please enter password.")
+                    return
+                }
+
+                setLoading(true)
+
+                self.signInToFirebase(email: emailText, password: passwordText)
+            }
+            
+    }
+    
+    func signInToFirebase(email: String, password: String) {
+        Auth.auth().signIn(
+            withEmail: email, password: password,
+            completion: { (result, error) in
+                if error == nil {
+                    print("login successful")
+                    self.setLoading(false)
+                    self.notificationCenter.post(name: .loggedIn, object: nil)
+                    self.onLoginSuccess?()
+                    let options = ["Option1", "Option2", "Option3"] // Replace with your actual options
+                    let selectedOption = "Option1" // Replace with the appropriate value
+                    let notificationName = NSNotification.Name("loggedIn")
+                    let landingVC = LandingPagePickerViewController(options: options, selectedOption: selectedOption, notificationName: notificationName)
+                    self.navigationController?.setViewControllers(
+                        [landingVC], animated: true)
+                } else {
+                    self.setLoading(false)
+                    print("Error occured: \(String(describing: error))")
+                    self.showAlert(
+                        title: "Error!", message: "Incorrect credentials")
+                }
+            })
+    }
+    
+    func setLoading(_ loading: Bool) {
+        loginView.loginButton.isEnabled = !loading
+        if loading {
+            loginView.activityIndicator.startAnimating()
+            loginView.loginButton.setTitle("", for: .normal)
+        } else {
+            loginView.activityIndicator.stopAnimating()
+            loginView.loginButton.setTitle("Login", for: .normal)
+        }
+    }
+    
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(
+            title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+
+}
--- NeatMeet/NeatMeet/ViewControllers/ViewController.swift
+++ NeatMeet/NeatMeet/ViewControllers/ViewController.swift
@@ -15,5 +15,205 @@ class ViewController: UIViewController {
     }
 
 
+<<<<<<< Updated upstream
+=======
+    private func configureUIElements() {
+        landingView.profileImage.menu = getProfileImageMenu()
+        landingView.eventTableView.delegate = self
+        landingView.eventTableView.dataSource = self
+        landingView.eventTableView.separatorStyle = .none
+        landingView.searchBar.delegate = self
+
+        let tapRecognizer = UITapGestureRecognizer(
+            target: self, action: #selector(hideKeyboardOnTap))
+        tapRecognizer.cancelsTouchesInView = false
+    }
+
+    @objc func hideKeyboardOnTap() {
+        view.endEditing(true)
+    }
+
+    private func configureButtonActions() {
+        landingView.addButton.addTarget(
+            self, action: #selector(navigateToCreatePost), for: .touchUpInside)
+        landingView.stateButton.addTarget(
+            self, action: #selector(stateButtonTapped), for: .touchUpInside)
+        landingView.stateDropButton.addTarget(
+            self, action: #selector(stateButtonTapped), for: .touchUpInside)
+        landingView.cityButton.addTarget(
+            self, action: #selector(cityButtonTapped), for: .touchUpInside)
+        landingView.cityDropButton.addTarget(
+            self, action: #selector(cityButtonTapped), for: .touchUpInside)
+    }
+
+    private func addNotificationCenter() {
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(handleStateSelected(notification:)),
+            name: .selectState, object: nil)
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(handleCitySelected(notification:)),
+            name: .selectCity, object: nil)
+    }
+
+    func getEvents() {
+        for i in 15..<25 {
+            let event = Event(
+                name: "Charles River \(i)",
+                likesCount: Int.random(in: 10...100),
+                datePublished: Date().addingTimeInterval(TimeInterval(i * -50000)),
+                publishedBy: "summer@gmail.com",
+                address: "123 Longwood Ave \(i)",
+                city: "City \(i)",
+                state: "State \(i % 5)",
+                imageUrl: "https://example.com/image\(i).jpg",
+                image: UIImage(named: "RiverCleaning")!
+            )
+            
+            events.append(event)
+        }
+        displayedEvents = events
+    }
+
+    func getAllEvents() async {
+            let docRef = db.collection("events").order(by: "datePublished", descending: false)
+            
+            do {
+                let snapshot = try await docRef.getDocuments()
+                events.removeAll()
+                
+                for document in snapshot.documents {
+                    let data = document.data()
+                    
+                    if let name = data["name"] as? String,
+                       let likesCount = data["likesCount"] as? Int,
+                       let timestamp = data["datePublished"] as? Timestamp,
+                       let address = data["address"] as? String,
+                       let city = data["city"] as? String,
+                       let state = data["state"] as? String,
+                       let imageUrl = data["imageUrl"] as? String,
+                       let publishedBy = data["publishedBy"] as? String{
+                        
+                        events.append(
+                            Event(
+                                name: name,
+                                likesCount: likesCount,
+                                datePublished: timestamp.dateValue(),
+                                publishedBy: publishedBy,
+                                address: address,
+                                city: city,
+                                state: state,
+                                imageUrl: imageUrl,
+                                image: UIImage(named: imageUrl)!
+                            )
+                        )
+                    }
+                }
+                
+                landingView.eventTableView.reloadData()
+                
+            } catch {
+                print("Error getting documents: \(error)")
+            }
+        }
+
+    @objc func navigateToCreatePost() {
+        let createPostViewController = CreatePostViewController()
+        navigationController?.pushViewController(
+            createPostViewController, animated: true)
+    }
+
+    func showLoginScreen() {
+        let loginVC = LoginViewController()
+        navigationController?.setViewControllers([loginVC], animated: true)
+    }
+
+    func getProfileImageMenu() -> UIMenu {
+        let menuItems = [
+            UIAction(
+                title: "Profile",
+                handler: { (_) in self.profileImageTapped() }),
+            UIAction(
+                title: "Logout",
+                handler: { (_) in self.logout() }),
+        ]
+        return UIMenu(title: "", children: menuItems)
+    }
+
+    func profileImageTapped() {
+        let profileController = ProfileViewController()
+        profileController.delegate = self
+        navigationController?.pushViewController(
+            profileController, animated: true)
+
+    }
+
+    func logout() {
+
+    }
+
+    @objc private func handleStateSelected(notification: Notification) {
+        let state = (notification.object as! String)
+        if state != selectedState {
+            selectedState = state
+            selectedCity = citiesByState[selectedState]?.first ?? ""
+            landingView.stateButton.setTitle(selectedState, for: .normal)
+            landingView.cityButton.setTitle(selectedCity, for: .normal)
+            filterEvents()
+        }
+    }
+
+    @objc private func handleCitySelected(notification: Notification) {
+        let city = (notification.object as! String)
+        selectedCity = city
+        landingView.cityButton.setTitle(selectedCity, for: .normal)
+        filterEvents()
+    }
+
+    private func filterEvents() {
+
+    }
+
+    func setUpBottomPickerSheet(
+        options: [String], selectedOption: String?,
+        notificationName: NSNotification.Name
+    ) {
+        let finalSelectedOption = selectedOption ?? options.first ?? ""
+
+        let pickerVC = LandingPagePickerViewController(
+            options: options, selectedOption: finalSelectedOption,
+            notificationName: notificationName
+        )
+
+        navController = UINavigationController(rootViewController: pickerVC)
+        navController?.modalPresentationStyle = .pageSheet
+
+        if let bottomPickerSheet = navController?.sheetPresentationController {
+            bottomPickerSheet.detents = [.medium()]
+
+            bottomPickerSheet.prefersGrabberVisible = false
+            navController?.isModalInPresentation = true
+        }
+    }
+
+    @objc func stateButtonTapped() {
+        setUpBottomPickerSheet(
+            options: states, selectedOption: selectedState,
+            notificationName: .selectState)
+        if let navController = navController {
+            present(navController, animated: true)
+        }
+    }
+
+    @objc func cityButtonTapped() {
+        let cities =
+            citiesByState[selectedState] ?? citiesByState["Massachusetts"] ?? []
+        setUpBottomPickerSheet(
+            options: cities, selectedOption: selectedCity,
+            notificationName: .selectCity)
+        if let navController = navController {
+            present(navController, animated: true)
+        }
+    }
+>>>>>>> Stashed changes
 }
 
